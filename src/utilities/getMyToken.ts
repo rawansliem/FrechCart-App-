e832a16e4b6935bdc238a8109ea0f0e39cdc4f93
rawansliem <rawansliem13@gmail.com>
2025-11-10 23:13:26 +0200

    up

--- src/utilities/getMyToken.ts
+++ src/utilities/getMyToken.ts
@@ -6,7 +6,7 @@ export default async function getMyToken(){
 try{
    const decodedtoken= 
          (await cookies()).get('next-auth.session-token')?.value||
-         (await cookies()).get('___Secure-next-auth.session-token')?.value;
+         (await cookies()).get('__Secure-next-auth.session-token')?.value;
          if(!decodedtoken)return null
 
          const token= 
